a57cc810f334b94b79dc0baf948663c1dd8897cb
Andre Elizondo <andre@chef.io>
2016-05-24 14:17:51 -0700

    all of them

--- packer/scripts/chef-server.sh
+++ packer/scripts/chef-server.sh
@@ -5,5 +5,3 @@ sudo opscode-push-jobs-server-ctl reconfigure
 sudo chef-server-ctl install chef-manage
 sudo chef-server-ctl reconfigure
 sudo chef-manage-ctl reconfigure --accept-license
-# chef-server-ctl user-create delivery eval user noreply@chef.io 'eval4me!' --filename /vagrant/delivery-user.pem 2>&1
-# chef-server-ctl org-create chefautomate 'chef automate evaluation' --filename /vagrant/chefautomate-validator.pem -a delivery 2>&1
