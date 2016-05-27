dfc50df4d0de3322df318fac81539a6b26bca777
Andre Elizondo <andre@chef.io>
2016-05-27 13:40:44 -0700

    quiet the wgets, change the azure image sizes

--- packer/scripts/chef-server.sh
+++ packer/scripts/chef-server.sh
@@ -2,7 +2,7 @@
 
 echo '127.0.0.1 chef-server.chef-automate.com' | sudo tee -a /etc/hosts
 sudo hostname chef-server.chef-automate.com
-wget https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_12.6.0-1_amd64.deb -O /tmp/chef-server-core_12.6.0-1_amd64.deb
+wget -q https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_12.6.0-1_amd64.deb -O /tmp/chef-server-core_12.6.0-1_amd64.deb
 sudo dpkg -i /tmp/chef-server-core_12.6.0-1_amd64.deb
 sudo chef-server-ctl install opscode-push-jobs-server
 sudo chef-server-ctl reconfigure
