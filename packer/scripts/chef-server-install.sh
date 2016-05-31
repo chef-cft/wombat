d11129814b1d8084549baac30b5e0ae0f6f1170c
Andre Elizondo <andre@chef.io>
2016-05-31 10:42:31 -0700

    place pregen cert on chef server

--- packer/scripts/chef-server-install.sh
+++ packer/scripts/chef-server-install.sh
@@ -5,6 +5,7 @@ sudo hostnamectl set-hostname chef-server
 wget -q https://packages.chef.io/stable/ubuntu/14.04/chef-server-core_12.6.0-1_amd64.deb -O /tmp/chef-server-core_12.6.0-1_amd64.deb
 sudo dpkg -i /tmp/chef-server-core_12.6.0-1_amd64.deb
 sudo mkdir -p /var/opt/opscode/nginx/ca/
+sudo cp /tmp/chef-server_chef-automate_com.crt /var/opt/opscode/nginx/ca/
 sudo chef-server-ctl install opscode-push-jobs-server
 sudo chef-server-ctl reconfigure
 sudo opscode-push-jobs-server-ctl reconfigure
