7b180736966b1a465343a32afaad666cc13da9f2
Seth Thomas <sthomas@chef.io>
2016-06-02 03:32:49 -0700

    Fix apt, jq, and vendor dirs

--- packer/scripts/delivery-install.sh
+++ packer/scripts/delivery-install.sh
@@ -2,7 +2,8 @@
 
 echo '127.0.0.1 delivery-server.chef-automate.com delivery-server' | sudo tee -a /etc/hosts
 sudo hostnamectl set-hostname delivery-server
-sudo apt-get install jq
+sudo wget -q https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/local/bin/jq
+sudo chmod +x /usr/local/bin/jq
 wget -q https://packages.chef.io/stable/ubuntu/12.04/chefdk_$CHEFDK_VER-1_amd64.deb -O /tmp/chefdk_$CHEFDK_VER-1_amd64.deb
 sudo dpkg -i /tmp/chefdk_$CHEFDK_VER-1_amd64.deb
 wget -q https://chef.bintray.com/current-apt/ubuntu/14.04/delivery_$DELIVERY_VER-1_amd64.deb -O /tmp/delivery_$DELIVERY_VER-1_amd64.deb
