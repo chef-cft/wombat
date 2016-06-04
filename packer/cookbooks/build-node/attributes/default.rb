499201f76a04da7c535514626f7f9c5c09842664
Seth Thomas <sthomas@chef.io>
2016-06-04 11:12:13 -0700

    lean into the cookbook

---
+++ packer/cookbooks/build-node/attributes/default.rb
@@ -0,0 +1,9 @@
+default['demo']['org'] = 'diprotodontia'
+default['demo']['domain'] = 'chordata.biz'
+default['demo']['enterprise'] = 'mammalia'
+
+default['demo']['hosts'] = {
+  'chef-server' => '172.31.54.10',
+  'delivery' => '172.31.54.11',
+  'build-node-1' => '172.31.54.12'
+}
--- packer/cookbooks/build-node/recipes/default.rb
+++ packer/cookbooks/build-node/recipes/default.rb
@@ -4,4 +4,46 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+chef_server_url = "https://chef-server.#{node['demo']['domain']}/organizations/#{node['demo']['org']}"
+
+file '/home/ubuntu/.ssh/authorized_keys' do
+  content IO.read('/tmp/public.pub')
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode '0755'
+  action :create
+end
+
+directory '/etc/chef'
+directory '/etc/chef/trusted_certs'
+ 
+%w(chef-server delivery compliance).each do |f|
+  file "/etc/chef/trusted_certs/#{f}_#{node['demo']['domain'].tr('.','_')}.crt" do
+    content IO.read("/tmp/#{f}.crt")
+    action :create
+  end
+end
+  
+file '/etc/chef/client.pem' do
+  content IO.read('/tmp/private.pem')
+  action :create
+end
+
+template '/etc/chef/client.rb' do
+  source 'client.erb'
+  variables(
+    chef_server_url: chef_server_url,
+    client_key: '/etc/chef/client.pem',
+    node_name: "build-node-#{node['demo']['node-number']}"
+  )
+end
+
+node['demo']['hosts'].each do |hostname, ipaddress|
+  hostsfile_entry ipaddress do
+    hostname  hostname
+    aliases   ["#{hostname}.#{node['demo']['domain']}"]
+    action    :create
+  end
+end
+
 include_recipe 'delivery_build::default'
