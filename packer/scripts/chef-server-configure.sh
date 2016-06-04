b117b10aae8a0178e87ed70703d7539a61d325f0
Seth Thomas <sthomas@chef.io>
2016-06-04 10:18:17 -0700

    Refactor the vendoring and fix a comment

--- packer/scripts/chef-server-configure.sh
+++ packer/scripts/chef-server-configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
-echo 'creating delivery user and chefautomate org'
+echo 'Setting up users, orgs, and permissions via Cheffish'
 sudo -E /opt/opscode/embedded/bin/chef-apply /tmp/create-user-org.rb
