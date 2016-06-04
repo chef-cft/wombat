1be60a2874e88a44b8275ff63ffb8a638bafe335
Seth Thomas <sthomas@chef.io>
2016-06-04 08:36:20 -0700

    Various fixes, adding delivery-cli, moving inline PS to cookbook

--- packer/cookbooks/workstation/attributes/default.rb
+++ packer/cookbooks/workstation/attributes/default.rb
@@ -2,8 +2,9 @@
 
 default['demo']['org'] = 'diprotodontia'
 default['demo']['domain'] = 'chordata.biz'
+default['demo']['enterprise'] = 'mammalia'
 default['demo']['chef_server_url'] = "https://chef-server.#{node['demo']['domain']}/organizations/#{node['demo']['org']}"
-default['demo']['delivery_url'] = "https://delivery.#{node['demo']['domain']}/e/#{node['demo']['org']}"
+default['demo']['delivery_url'] = "https://delivery.#{node['demo']['domain']}/e/#{node['demo']['enterprise']}"
 
 default['demo']['hosts'] = {
   'chef-server' => '172.31.54.10',
