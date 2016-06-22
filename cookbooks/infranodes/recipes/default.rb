7bdaf0545c8483abca81ffe8404695d26c9174f1
Andre Elizondo <andre@chef.io>
2016-06-22 13:33:45 -0700

    add back enterprise from datacollector

--- cookbooks/infranodes/recipes/default.rb
+++ cookbooks/infranodes/recipes/default.rb
@@ -18,7 +18,7 @@ end
 include_recipe 'push-jobs'
 
 chef_server_url = "https://chef-server.#{node['demo']['domain']}/organizations/#{node['demo']['org']}"
-delivery_server_url = "https://delivery.#{node['demo']['domain']}"
+delivery_server_url = "https://delivery.#{node['demo']['domain']}/e/#{node['demo']['enterprise']}"
 
 template '/etc/chef/client.rb' do
   source 'client.rb.erb'
