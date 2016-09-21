35d24f5b18a29707465caf6b78e8d61e2cb6d225
Andre Elizondo <andre@chef.io>
2016-09-20 17:04:05 -0700

    initial windows support

--- cookbooks/infranodes/recipes/default.rb
+++ cookbooks/infranodes/recipes/default.rb
@@ -4,20 +4,30 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-apt_update 'packages' do
-  action :update
-  only_if { node['platform_family'] == 'debian' }
+unless nodes['platform'] == 'windows'
+  apt_update 'packages' do
+    action :update
+    only_if { node['platform_family'] == 'debian' }
+  end
 end
-
+  
 chef_ingredient 'chef' do
   channel node['demo']['versions']['chef'].split('-')[0].to_sym
   version node['demo']['versions']['chef'].split('-')[1]
   action :install
 end
 
-directory '/etc/chef'
+if node['platform'] == 'windows'
+  conf_dir = "C:/chef"
+  tmp_dir = "C:/Windows/Temp"
+else
+  conf_dir = "/etc/chef"
+  tmp_dir = "/tmp"
+end
 
-template '/etc/chef/client.rb' do
+directory conf_dir
+
+template File.join(conf_dir, 'client.rb') do
   source 'client.rb.erb'
   variables({
       :chef_server_url => node['demo']['chef_server_url'],
@@ -26,16 +36,16 @@ template '/etc/chef/client.rb' do
   })
 end
 
-file '/etc/chef/client.pem' do
-  content lazy { IO.read('/tmp/private.pem') }
+file File.join(conf_dir, 'client.pem') do
+  content lazy { IO.read(File.join(tmp_dir, 'private.pem')) }
 end
 
 ###todo: centralize this into the wombat cookbook
-directory '/etc/chef/trusted_certs'
+directory File.join(conf_dir, 'trusted_certs')
 
 %w(chef automate compliance).each do |f|
-  file "/etc/chef/trusted_certs/#{node['demo']['domain_prefix']}#{f}_#{node['demo']['domain'].tr('.','_')}.crt" do
-    content lazy { IO.read("/tmp/#{f}.crt") }
+  file File.join(conf_dir, "trusted_certs/#{node['demo']['domain_prefix']}#{f}_#{node['demo']['domain'].tr('.','_')}.crt") do
+    content lazy { IO.read(File.join(tmp_dir, "#{f}.crt")) }
   end
 end
 ###
